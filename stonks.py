18967ffa0441ea512b174761d5b5e4a2e944fa17
Yuanfeng123 <61365068+Yuanfeng123@users.noreply.github.com>
2022-07-25 10:15:41 -0400

    Add files via upload

--- stonks.py
+++ stonks.py
@@ -43,9 +43,9 @@ class Solution:
 
         # TODO: Write code below to return an int with the solution to the prompt
         ans = 0
-        for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
-                ans = max(ans, prices[j] - prices[i])
+        for i in range(1,len(prices)):
+            if prices[i] - prices[i-1] >0:
+                ans+=(prices[i] - prices[i-1])
         return ans
 
 
